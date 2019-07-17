c081bfdb77e4e1d32b516e2a4d7d799ef714532c
jvalenzuela <jvalenzuela@indeed.com>
2019-07-17 11:09:24 +0100

    first commit tests

--- testing/__init__.py
+++ testing/__init__.py
@@ -5,7 +5,7 @@ import pandas as pd
 from nose.tools import with_setup
 from contextlib import contextmanager
 import mock
-from fido.testing.testingutil import temp_sys_args, mock_eravana_config
+from fido.testing.testingutil import temp_sys_args
 
 
 sys.path = [abspath(join(dirname(__file__), pardir, 'mgo'))] + sys.path
@@ -14,6 +14,18 @@ from transform import CronGDWTransform
 
 CONFIG_FILE = abspath(join(dirname(__file__), pardir,
                            'tieredconf', 'secrets.properties'))
+
+
+def load_config_for_eravana_tst(config_file=CONFIG_FILE):
+    config = load_tiered_config(config_file)
+    return config
+
+
+@contextmanager
+def mock_eravana_config():
+    with mock.patch('cmdlineutil.tieredconfig.load_tiered_config') as mock_tieredconf:
+        mock_tieredconf.side_effect = load_config_for_eravana_tst
+        yield mock_tieredconf
 
 
 def setup_func():
@@ -31,8 +43,9 @@ def test_simple_split():
     args = 'transform.py --props "{}" -t test/account_sales_rep -s "2018-03-01" -e "2018-03-02"'.format(CONFIG_FILE)
     args = args.split()
     with temp_sys_args(*args), mock_eravana_config():
-        app = CronGDWTransform(props=self.config)
+        app = CronGDWTransform()
     from nose.tools import set_trace; set_trace()
+    print(app.props)
     app.run()
     chase_ract0010 = pd.DataFrame({
         'merchant_order_num': ['1', '2', '3', '4']
